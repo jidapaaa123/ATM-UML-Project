869ed68d47393b88f71653ca2b399e101cf065af
Jidapa Angsutti <jidapa.angsutti@students.snow.edu>
2025-03-21 11:22:05 -0600

    updated ATM

--- Logic/ATM.cs
+++ Logic/ATM.cs
@@ -23,20 +23,20 @@ namespace Logic
 			}
 			return false;
 		}
-		public bool requestAmount() {
+		public void requestAmount() {
 			Console.Write("Enter the amount you want to withdraw: ");
             if (!double.TryParse(Console.ReadLine(), out double amount)) {
                 Console.WriteLine("Invalid amount. Please try again.");
-                return false;
+                return;
             } else if (amount <= 0) {
                 Console.WriteLine("Amount must be greater than 0. Please try again.");
-                return false;
+                return;
             } else if (amount > bankServer.checkBalance(currentCardNumber)) {
                 Console.WriteLine("Insufficient funds. Please try again.");
-                return false;
+                return;
             } else {
                 bankServer.processTransaction(currentCardNumber, amount);
-                return true;
+                return;
             }
         }
 		public void dispenseCash() {
